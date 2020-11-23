5bd55761e69ed9c7f35760e223b0901b0e8424d4
DOCENTE <docente@rosselli.loc>
2020-11-23 10:26:45 +0100

    Add project files.

---
+++ NumeriPerfetti/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace NumeriPerfetti
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            int i = 1;
+            int somma = 0;
+
+            Console.WriteLine("Inserisci un numero");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            while (i < numero)
+            {
+                if(numero%i == 0)
+                {
+                    somma = somma + i;
+                }
+                i++;
+            }
+
+            if(numero == somma)
+            {
+                Console.WriteLine("è perfetto");
+            }
+            else
+            {
+                Console.WriteLine("non è perfetto");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
